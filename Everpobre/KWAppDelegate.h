c8cb321c4da6eb3cf4e594caee26114dcda9d74b
ANDRES KWAN <andreskwan@gmail.com>
2013-05-09 14:15:20 -0500

    Initial Commit

---
+++ Everpobre/KWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KWAppDelegate.h
+//  Everpobre
+//
+//  Created by Andres Kwan on 5/9/13.
+//  Copyright (c) 2013 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Everpobre/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Everpobre
+//
+//  Created by Andres Kwan on 5/9/13.
+//  Copyright (c) 2013 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KWAppDelegate class]));
+    }
+}
